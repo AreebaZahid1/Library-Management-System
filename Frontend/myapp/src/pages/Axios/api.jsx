6707860d0aa15b4f5c1e4d91c08e844629bbb66e
AreebaZahid1 <areebabilal00@gmail.com>
2026-09-09 19:44:16 +0500

    Configure frontend API

--- Frontend/myapp/src/pages/Axios/api.jsx
+++ Frontend/myapp/src/pages/Axios/api.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-      process.env.REACT_APP_API_URL || "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
 });
 
-API.interceptors.request.use(
+api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-    } 
+    }
+
     return config;
   },
   (error) => {
